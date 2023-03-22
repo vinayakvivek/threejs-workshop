128d0bce1953452d1c55e5f5bccbd3bfddf3e22b
Vinayak <vinayakdeepu@gmail.com>
2023-03-22 11:11:46 +0530

    Create renderer, scene and camera

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-import './style.css'
+import "./style.css";
+
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.setZ(5);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#canvas"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// main render loop
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+// start rendering
+render();
