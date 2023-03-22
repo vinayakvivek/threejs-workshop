47cefded8375410ba0677a973231f169ce9489a9
Vinayak <vinayakdeepu@gmail.com>
2023-03-22 11:14:27 +0530

    Add a basic box for testing scene

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const box = new THREE.Mesh(geometry, material);
+scene.add(box);
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
